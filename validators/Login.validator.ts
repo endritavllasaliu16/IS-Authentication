621370eaa1c2844557a109c9104e20898809a3cb
Dijane Azemi <93864066+dijanazemi@users.noreply.github.com>
2023-01-05 22:35:37 +0100

    Delete validators directory

--- validators/Login.validator.ts
+++
@@ -1,6 +0,0 @@
-import Joi from "joi";
-
-export const LoginValidator = Joi.object().keys({
-    email: Joi.string().email({ tlds: false }).required(),
-    password: Joi.string().required(),
-});
--- validators/Register.validator.ts
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-export const RegisterValidator = Joi.object().keys({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    password: Joi.string().required(),
-});
